6ef2d59ac83882d4903ccf610b260c4cb8d75f90
imtritesh <103854328+imtritesh@users.noreply.github.com>
2023-03-24 11:11:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function threeSum(arr, target) {
 // write your code here
+	var sum = 0;
+	var minDiff = Number.MAX_VALUE;;
+	for(var i=0; i<arr.length; i++){
+		for(var j=i; j<=i+2; j++){
+			sum = sum + arr[j];
+		}
+		if(sum<=minDiff){
+			minDiff === sum;
+		}
+	}
+	return minDiff;
   
 }
 
