ffa007e4e0dbb999505919052434e7f4824caed1
imtritesh <103854328+imtritesh@users.noreply.github.com>
2023-05-12 01:49:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 function threeSum(S, target) {
-	let sum = 0;
-	let n = S.length;
-	let diff = Number.MAX_VALUE;
-	let ans = sum - target;
-	for(let i=0; i<n-2; i++){
-		for(let j=i; j<i+2; j++){
-			sum += S[j];
-		}
-		if(ans<diff){
-			diff = ans;
-		}
-	}
-	return diff;
+  S.sort((a, b) => a - b); // Sort the array in ascending order
+  let closestSum = S[0] + S[1] + S[2]; // Initialize closest sum with the sum of first three elements
+  for (let i = 0; i < S.length - 2; i++) {
+    let left = i + 1;
+    let right = S.length - 1;
+    while (left < right) {
+      const sum = S[i] + S[left] + S[right];
+      if (Math.abs(target - sum) < Math.abs(target - closestSum)) {
+        closestSum = sum; // Update closest sum
+      }
+      if (sum < target) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
+  return closestSum;
 }
